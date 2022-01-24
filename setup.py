33668825db323a1e51a261aadf2b37b45001e594
Michael Holbert Roberts <mhr320@gmail.com>
2022-01-24 16:50:59 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(here / 'README.md', 'r') as f:
 
 setup(
     name="Wmt2Ics",
-    version="0.0.22",
+    version="0.0.23",
     author="M. Holbert Roberts",
     author_email="mhr320@gmail.com",
     description="Converts wmt schedule Views:My Schedule to .ics file",
@@ -26,5 +26,5 @@ setup(
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
     ],
-    python_requires='>=3, <4'
+    python_requires='>=3.8, <4'
     )
